576166de7218f110d6d7f2bdbf598058f9ab3c3e
gizmore <gizmore@wechall.net>
2024-12-28 16:06:39 +0100

    Revert "'pytest migration. async mysql'"
    This reverts commit 24e08045921dc00f2ab5f1009d84b4d1ffd84d9b.

--- module_telegram.py
+++ module_telegram.py
@@ -49,8 +49,10 @@ class module_telegram(GDO_Module):
     def cfg_api_key(self) -> str:
         return self.get_config_val('telegram_api_key')
 
-    async def gdo_init(self):
+    def gdo_init(self):
         Connector.register(Telegram, True)
+        if not Application.IS_HTTP:
+            nest_asyncio.apply()
 
     def gdo_install(self):
         if not GDO_Server.get_by_connector('Telegram'):
